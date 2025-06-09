d96e29e2dcbc13b786451e4d2ab3b813ad385b0f
pletzer <pletzer@users.noreply.github.com>
2025-06-09 23:14:57 +0000

    deploy: 3539ee2a8904760ac94cbde50ee890fb54df8c73

--- search/all_6.js
+++ search/all_6.js
@@ -3,8 +3,8 @@ var searchData=
   ['get_0',['get',['../classCmdLineArgParser.html#a2a30ea22a2d087e8317473307c8d27f5',1,'CmdLineArgParser']]],
   ['getarrayfromstate_1',['getArrayFromState',['../classSeapodymCohortAbstract.html#afcabc8d41e389a745d882b128fd79554',1,'SeapodymCohortAbstract::getArrayFromState()'],['../classSeapodymCohortFake.html#aa68cfe739b4d29154aa7c1f5a1e8e87b',1,'SeapodymCohortFake::getArrayFromState()']]],
   ['getdataptr_2',['getDataPtr',['../classSeapodymCourier.html#ac63cac5667d7f923915b74b96fbdc882',1,'SeapodymCourier']]],
-  ['getdependencies_3',['getDependencies',['../classSeapodymTaskManager.html#a3230a6fd3b713dc7e026fb6e0c2f2901',1,'SeapodymTaskManager']]],
-  ['getinittaskids_4',['getInitTaskIds',['../classSeapodymTaskManager.html#a8b7044e4972c24e9ff4cdcb28c2ad53b',1,'SeapodymTaskManager']]],
-  ['getnexttask_5',['getNextTask',['../classSeapodymTaskManager.html#a6851ee6dfbeb0ff23c44280a73c3e25b',1,'SeapodymTaskManager']]],
-  ['getnumsteps_6',['getNumSteps',['../classSeapodymTaskManager.html#a8a98b3a68ec04a28d09cdd9c7441ddfb',1,'SeapodymTaskManager']]]
+  ['getdependencies_3',['getDependencies',['../classSeapodymCohortManager.html#ae15701826cfb8393928ca0d0d2af49f5',1,'SeapodymCohortManager']]],
+  ['getinitcohortids_4',['getInitCohortIds',['../classSeapodymCohortManager.html#a8d90c24a146c23af511eb06fbc942c6a',1,'SeapodymCohortManager']]],
+  ['getnextcohort_5',['getNextCohort',['../classSeapodymCohortManager.html#a5df2ede3e48e612d6673338dfdd5b796',1,'SeapodymCohortManager']]],
+  ['getnumsteps_6',['getNumSteps',['../classSeapodymCohortManager.html#ae34f18f3427e70e294759cffa82850a9',1,'SeapodymCohortManager']]]
 ];
--- search/classes_1.js
+++ search/classes_1.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['seapodymcohortabstract_0',['SeapodymCohortAbstract',['../classSeapodymCohortAbstract.html',1,'']]],
   ['seapodymcohortfake_1',['SeapodymCohortFake',['../classSeapodymCohortFake.html',1,'']]],
-  ['seapodymcourier_2',['SeapodymCourier',['../classSeapodymCourier.html',1,'']]],
-  ['seapodymtaskmanager_3',['SeapodymTaskManager',['../classSeapodymTaskManager.html',1,'']]]
+  ['seapodymcohortmanager_2',['SeapodymCohortManager',['../classSeapodymCohortManager.html',1,'']]],
+  ['seapodymcourier_3',['SeapodymCourier',['../classSeapodymCourier.html',1,'']]]
 ];
