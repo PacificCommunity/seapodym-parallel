d640d9d8ac5ce97956723d4152adbbc6ac98cc9f
pletzer <pletzer@users.noreply.github.com>
2025-06-10 22:49:33 +0000

    deploy: 6cf6ea173fb3fbec42d7ad388d77a571c42bd02a

--- search/all_0.js
+++ search/all_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['addfootnote_0',['addFootnote',['../classCmdLineArgParser.html#a0dd5c09e1b50be309f8ea56a1da13545',1,'CmdLineArgParser']]]
+  ['accumulate_0',['accumulate',['../classSeapodymCourier.html#a70897f532e325d638efb8f78301a7caf',1,'SeapodymCourier']]],
+  ['addfootnote_1',['addFootnote',['../classCmdLineArgParser.html#a0dd5c09e1b50be309f8ea56a1da13545',1,'CmdLineArgParser']]]
 ];
--- search/functions_4.js
+++ search/functions_4.js
@@ -5,6 +5,7 @@ var searchData=
   ['getdataptr_2',['getDataPtr',['../classSeapodymCourier.html#ac63cac5667d7f923915b74b96fbdc882',1,'SeapodymCourier']]],
   ['getdependencies_3',['getDependencies',['../classSeapodymCohortManager.html#ae15701826cfb8393928ca0d0d2af49f5',1,'SeapodymCohortManager']]],
   ['getinitcohortids_4',['getInitCohortIds',['../classSeapodymCohortManager.html#a8d90c24a146c23af511eb06fbc942c6a',1,'SeapodymCohortManager']]],
-  ['getnextcohort_5',['getNextCohort',['../classSeapodymCohortManager.html#a5df2ede3e48e612d6673338dfdd5b796',1,'SeapodymCohortManager']]],
-  ['getnumsteps_6',['getNumSteps',['../classSeapodymCohortManager.html#ae34f18f3427e70e294759cffa82850a9',1,'SeapodymCohortManager']]]
+  ['getnewcohortworker_5',['getNewCohortWorker',['../classSeapodymCohortManager.html#af7c1590afbfa2a86e5e1384a98dc3443',1,'SeapodymCohortManager']]],
+  ['getnextcohort_6',['getNextCohort',['../classSeapodymCohortManager.html#a5df2ede3e48e612d6673338dfdd5b796',1,'SeapodymCohortManager']]],
+  ['getnumsteps_7',['getNumSteps',['../classSeapodymCohortManager.html#ae34f18f3427e70e294759cffa82850a9',1,'SeapodymCohortManager']]]
 ];
