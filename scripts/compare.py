50d27a48b2203e514597654412f9b91c5420867f
Alex Pletzer <alexander@gokliya.net>
2025-07-01 16:20:53 +1200

    work in progress

--- scripts/compare.py
+++ scripts/compare.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import numpy as np
 import defopt
+import re
+
+pat = re.compile(r'^\[(\d+)\]\s+@\s+step\:\s+(\d+)\s+checksum\:\s*([\d\\.eE\-\+]+)')
 
 def load(output):
     res = {}
@@ -22,13 +25,22 @@ def main(*, output1: str, output2: str):
     """
     checksum1 = load(output1)
     checksum2 = load(output2)
+    print(checksum1)
+    print(checksum2)
+    nerrs = 0
     print(f'comparing 1 against 2...')
     for procId in checksum1:
         for step in checksum1[procId]:
             c1 = checksum1[procId][step]
-            c2 = checksum2[procId][step]
+            try:
+                c2 = checksum2[procId][step]
+            except:
+                print(f'could not retrieve [{procId}] @ step {step} from file {output2}!!')
+                c2 = -99999
             if c1 != c2:
                 print(f'[{procId}] @ step {step}: {c1} != {c2}')
+                nerrs += 1
+    print(f'number of differences: {nerrs}')
 
 if __name__ == '__main__':
     defopt.run(main)
