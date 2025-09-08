78d6ba6a57909c4794d5c99e048717b134dd6681
Alex Pletzer <alexander@gokliya.net>
2025-09-08 16:06:36 +1200

    turning off the MPI_Win_sync

--- src/TaskStepManager.cpp
+++ src/TaskStepManager.cpp
@@ -71,7 +71,7 @@ TaskStepManager::run() const {
                 int chunk_id = this->chunkIdMap.at(key);
 
                 // ensure local window is up to date
-                MPI_Win_sync(dataCollect->win);
+                //MPI_Win_sync(dataCollect->win);
                 
                 double lastVal = managerData[chunk_id * numData + (numData - 1)];
                 if (lastVal != this->dataCollect->BAD_VALUE) {
