245b81dd07289e7873117b5e2b96e47305f673d2
Alexander Pletzer <alexander@gokliya.net>
2025-09-10 07:29:33 +1200

    adding results

--- report/cohort-parallelisation/results/plot_results.py
+++ report/cohort-parallelisation/results/plot_results.py
@@ -12,7 +12,7 @@ data = {
     'speedup_std': [],
 }
 
-for f in glob.glob("nm*_nd*/resultsNw*.csv"):
+for f in glob.glob("resultsNw*.csv"):
     print(f)
     workers = int(re.search(r'Nw(\d+)', f).group(1))
     num_doubles = int(re.search(r'nm(\d+)_nd(\d+)', f).group(2))
@@ -32,22 +32,19 @@ print(df)
 #sns.set_theme(style="whitegrid")
 plt.figure(figsize=(10, 6))
 
-#sns.lineplot(data=df, x='workers', y='speedup_avg', hue='num_doubles', marker='o', err_style="bars", ci='sd')
-df1 = df[(df['num_doubles'] == 10000) & (df['step_milliseconds'] == 100)].sort_values(by='workers', ascending=True)
-x = df1['workers']
-y = df1['speedup_avg']
-yerr = df1['speedup_std']
-plt.errorbar(x, y, yerr=yerr, label='10000 doubles/100ms per step', marker='o', capsize=5)
+for nd in df['num_doubles'].unique():
+    for nm in df['step_milliseconds'].unique():
+        df1 = df[(df['num_doubles'] == nd) & (df['step_milliseconds'] == nm)].sort_values(by='workers', ascending=True)
+        x = df1['workers']
+        y = df1['speedup_avg']
+        yerr = df1['speedup_std']
+        plt.errorbar(x, y, yerr=yerr, label=f'{nd} init dbl, {nm}ms step', marker='o', capsize=5)
 
-df2 = df[(df['num_doubles'] == 15000) & (df['step_milliseconds'] == 10)].sort_values(by='workers', ascending=True)
-x = df2['workers']
-y = df2['speedup_avg']
-yerr = df2['speedup_std']
-plt.errorbar(x, y, yerr=yerr, label='15000 doubles/10ms per step', marker='x', capsize=5)
+plt.plot([df['workers'].min(), df['workers'].max()], [df['workers'].min(), df['workers'].max()], 'k--')
 
-plt.title('Speedup vs Number of Workers')
-plt.xlabel('Number of Workers')
+plt.title('Parallel scalability')
+plt.xlabel('# workers = # age groups')
 plt.ylabel('Speedup')
-plt.legend(title='Number of Doubles')
+plt.legend(title='')
 plt.savefig('speedup_vs_workers.png')
 plt.show()
