85ab3cde7e5a9fe7b8172ef9f5b0f540ff044796
Alexander Pletzer <alexander@gokliya.net>
2025-09-17 11:26:28 +1200

    newest numbers

--- report/cohort-parallelisation/results_seapodym/plot_scaling.py
+++ report/cohort-parallelisation/results_seapodym/plot_scaling.py
@@ -1,13 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 plt.rcParams.update({'font.size': 12}) # Set the global font size to 18
 
-nprocs = np.array([2, 4, 7, 10, 19, 37])
-nworkers = nprocs - 1
-manager_times = np.array([59.6544, 18.0884, 7.21377, 5.128, 2.89823, 1.98355])
+data = pd.read_csv('timings.csv')
+
+nworkers = data.nw
+manager_times = data.time_manager
 
 plt.plot(nworkers, manager_times[0]/manager_times)
+plt.plot(nworkers, manager_times[0]/manager_times, 'ko')
 plt.plot([1, 36], [1, 36], 'k--')
 plt.xlabel('number of workers')
 plt.ylabel('speedup relative to 1 worker')
