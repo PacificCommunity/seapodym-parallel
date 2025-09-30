c62f6b8e31544e3b984e83e698dffd8733bc6a00
Alexander Pletzer <alexander@gokliya.net>
2025-09-30 14:02:27 +1300

    code cleaning

--- src/TaskStepWorker.cpp
+++ src/TaskStepWorker.cpp
@@ -18,13 +18,9 @@ TaskStepWorker::TaskStepWorker(MPI_Comm comm,
 void
 TaskStepWorker::run() const {
 
-    const int manager_rank = 0;
-    int workerId;
-    MPI_Comm_rank(this->comm, &workerId);
-
     const int managerRank = 0;
 
-    std::string sworkerId = std::to_string(workerId);
+    std::string sworkerId = std::to_string(this->rank);
     // Use true to let logs be overwritten, otherwise the logs will be appended
     auto logger = spdlog::basic_logger_mt(sworkerId, "log_worker" + sworkerId + ".txt", true);
     logger->set_level(spdlog::level::debug);
