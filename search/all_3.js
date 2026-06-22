dca171bf3d44c331c3a11d89d624321def7b7aad
pletzer <pletzer@users.noreply.github.com>
2026-06-22 01:47:16 +0000

    deploy: ab72e1e717761ddac701b1ab77f5de650244d464

--- search/all_3.js
+++ search/all_3.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['dataprovider_0',['DataProvider',['../classDataProvider.html',1,'DataProvider'],['../classDataProvider.html#a505690c3fd3c522e2e21b00b0e198846',1,'DataProvider::DataProvider()']]],
   ['dephash_1',['DepHash',['../structDepHash.html',1,'']]],
-  ['distdatacollector_2',['DistDataCollector',['../classDistDataCollector.html',1,'DistDataCollector'],['../classDistDataCollector.html#addc699fa6632609c66c865671cb1e2bc',1,'DistDataCollector::DistDataCollector()']]],
+  ['distdatacollector_2',['DistDataCollector',['../classDistDataCollector.html',1,'DistDataCollector'],['../classDistDataCollector.html#af8c0c8f69f91e605f252ad20f10d3519',1,'DistDataCollector::DistDataCollector()']]],
   ['documentation_3',['Documentation',['../index.html#autotoc_md4',1,'']]],
   ['documention_4',['How to build the documention',['../index.html#autotoc_md3',1,'']]]
 ];
--- search/functions_2.js
+++ search/functions_2.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['dataprovider_0',['DataProvider',['../classDataProvider.html#a505690c3fd3c522e2e21b00b0e198846',1,'DataProvider']]],
-  ['distdatacollector_1',['DistDataCollector',['../classDistDataCollector.html#addc699fa6632609c66c865671cb1e2bc',1,'DistDataCollector']]]
+  ['distdatacollector_1',['DistDataCollector',['../classDistDataCollector.html#af8c0c8f69f91e605f252ad20f10d3519',1,'DistDataCollector']]]
 ];
