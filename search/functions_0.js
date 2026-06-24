0c860130556aeee13f534f953a06c484fe625f51
pletzer <pletzer@users.noreply.github.com>
2026-06-24 22:01:13 +0000

    deploy: 1c937e72965510f2021e04b595ebf17198455432

--- search/functions_0.js
+++ search/functions_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['accumulate_0',['accumulate',['../classSeapodymCourier.html#a453fef0199d79d5904b544f1ce15c6c3',1,'SeapodymCourier']]],
+  ['accumulate_0',['accumulate',['../classDistDataCollector.html#ae2feb11f05abdae66ee954cfa9ad7ea6',1,'DistDataCollector::accumulate()'],['../classSeapodymCourier.html#a453fef0199d79d5904b544f1ce15c6c3',1,'SeapodymCourier::accumulate()']]],
   ['adddependencies_1',['addDependencies',['../classTaskDependencyManager.html#ac6c593329a2284338ab9b2b63a29c9ce',1,'TaskDependencyManager']]],
   ['addfootnote_2',['addFootnote',['../classCmdLineArgParser.html#a0dd5c09e1b50be309f8ea56a1da13545',1,'CmdLineArgParser']]]
 ];
